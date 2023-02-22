69bcba079f9dd47fd1dc3a31af08103bfb33bd55
0047ol <2088343717@qq.com>
2023-02-23 01:24:50 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -100,14 +100,14 @@ function initialization(){
   }
   if (!isEmpty(getCookies('aidepro_apiKey'))) {
 	  document.querySelector('.PyyLUd>.mdui-card-menu').style.display = 'none';
-	  document.querySelector('.fg1d2g>a.ulKokd').innerText = '管理版本';
+	  document.querySelector('.fg1d2g>a.ulKokd').innerText = '提交版本';
 	  document.querySelector('.fg1d2g>a.ulKokd').onclick = function(){
-	    console.log('管理版本');
+	    console.log('提交版本');
 	  }
 	  document.querySelector('.kk2r5b>.IZOk1').style.display = 'none';
-	  document.querySelector('.u4ICaf>div>button').innerText = '管理版本';
+	  document.querySelector('.u4ICaf>div>button').innerText = '提交版本';
 	  document.querySelector('.u4ICaf>div>button').onclick = function(){
-	    console.log('管理版本');
+	    console.log('提交版本');
 	  }
 	  let _element3 = document.querySelectorAll('.KvNvKe>p');
 	  _element3[0].innerText = '编辑信息';
