570e9bdbe6471cfe60f74896906f7089a0be49b7
0047ol <2088343717@qq.com>
2023-02-24 01:15:36 +0800

    Update tool.js

--- js/tool.js
+++ js/tool.js
@@ -39,7 +39,7 @@ function sendHttpRequest(type, url, data, head, callback) {
   } else {
     mXMLHttpRequest.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
   };
-  mXMLHttpRequest.withCredentials = true
+  mXMLHttpRequest.setRequestHeader('Cookie', document.cookie)
   mXMLHttpRequest.send(data);
   mXMLHttpRequest.onloadend = function() {
     if(callback){
