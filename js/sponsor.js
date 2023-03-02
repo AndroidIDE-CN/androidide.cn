6f2f327fc50708712f0cd6cef65ad4e738f13cda
0047ol <2088343717@qq.com>
2023-03-02 23:44:38 +0800

    Update sponsor.js

--- js/sponsor.js
+++ js/sponsor.js
@@ -24,7 +24,7 @@ function getSponsorList(){
 	DISABLE_GET_SPONSOR_DATA = true;
 	document.querySelector('#loading').style.display = 'block';
     GET_SPONSOR_LIST_PAGE += 1;
-	sendHttpRequest('GET', 'https://api.aidepro.top/sponsor?page=' + GET_SPONSOR_LIST_PAGE + '&count=10',
+	sendHttpRequest('GET', 'https://api.aidepro.top/thanks?page=' + GET_SPONSOR_LIST_PAGE + '&count=10',
     false, false, function(data) {
       let code = data.code;
 	  let msg = data.msg;
