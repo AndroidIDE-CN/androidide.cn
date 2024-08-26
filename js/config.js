d30abb5c97e1bc98daa5078421bb99b6cc8cc49b
AndroidIDE-Team <admin@androidide.cn>
2024-08-26 14:51:55 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,19 @@
+const WEB_LIST = [
+  {
+        icon: 'https://guild-1251316161.file.myqcloud.com/guild-share/qq-icon.png',
+        name: 'Group',
+        url: 'https://qm.qq.com/q/ghW82lhRwA',
+        desc: 'QQ群聊'
+    },
+    {
+        icon: 'https://guild-1251316161.file.myqcloud.com/guild-share/qq-icon.png',
+        name: 'Guild',
+        url: 'https://pd.qq.com/s/9hsaii9e4',
+        desc: 'QQ频道'
+    }
+];
+const FILTE_LIST = [
+    562699268,
+    756153568,
+    843243800
+];
